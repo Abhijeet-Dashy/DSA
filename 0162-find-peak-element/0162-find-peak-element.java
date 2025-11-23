427df1af0dd35ef1b428d61db51becdbc5e29074
Abhijeet Dash <abhijeetdashy@gmail.com>
2025-11-23 18:55:42 +0530

    Time: 0 ms (100%), Space: 44.3 MB (8.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,26 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            
-            if (nums[mid] < nums[mid + 1]) {
-                // slope goes up → peak is to the right
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if(n == 1) return 0;
+        if(arr[0] > arr[1]){
+            return 0;
+        }
+        if(arr[n-1] > arr[n-2]){
+            return n-1;
+        }
+        int low = 1, high = n-2;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1]){
                 low = mid + 1;
-            } else {
-                // slope goes down or mid is a peak → stay left
-                high = mid;
+            }
+            else{ 
+                high = mid - 1;
             }
         }
-        
-        return low; // low == high, position of a peak
+    return -1;
     }
 }
