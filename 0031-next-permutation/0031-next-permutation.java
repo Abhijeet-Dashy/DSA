d19d486f64b380802a74b1bd71375d62fdc62b5f
Abhijeet Dash <abhijeetdashy@gmail.com>
2026-02-09 21:02:39 +0530

    Time: 1 ms (11.98%), Space: 44.6 MB (66.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,44 +1,35 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int ind = -1;
-
-        // Step 1: Find the first decreasing element from the right
-        for (int i = n - 2; i >= 0; i--) {
+        boolean isSorted = true;
+       for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                ind = i;
+
+                int j = n - 1;
+                while (nums[j] <= nums[i]) {
+                    j--;
+                }
+                swap(nums, i, j);
+                reverse(nums, i + 1, n - 1);
+                isSorted = false;
                 break;
             }
-        }
-
-        // Step 2: If no such index, the array is the last permutation
-        if (ind == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        // Step 3: Find element just larger than nums[ind] to the right
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums, i, ind);
-                break;
+            else{
+                continue;
             }
         }
-
-        reverse(nums, ind + 1, n - 1);
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+        if(isSorted){
+            Arrays.sort(nums);
         }
     }
-
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
     }
 }
